2e1f92be3625b50a320fed1f9383937eac0ae770
mariodmpereira <mariodmpereira@gmail.com>
2022-07-09 01:14:45 +0100

    Create Spring application with the correct dependencies to generate OpenAPI definitions. Target was also commited for reference.

---
+++ openapi/openapi-barebones/src/main/java/com/mariodmpereira/openapi/barebones/OpenapiBarebonesApplication.java
@@ -0,0 +1,13 @@
+package com.mariodmpereira.openapi.barebones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiBarebonesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapiBarebonesApplication.class, args);
+    }
+
+}
